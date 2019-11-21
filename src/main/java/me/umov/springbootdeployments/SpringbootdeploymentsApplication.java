8d92a1124814fe587da2046530cfadf234b92b2c
robsonbittencourt <robson.luizv@gmail.com>
2019-11-20 21:57:50 -0300

    Add Lambda integration example

--- src/main/java/me/umov/springbootdeployments/SpringbootdeploymentsApplication.java
+++ src/main/java/me/umov/springbootdeployments/SpringbootdeploymentsApplication.java
@@ -1,10 +1,29 @@
 package me.umov.springbootdeployments;
 
+import me.umov.springbootdeployments.controller.BookController;
+import me.umov.springbootdeployments.controller.StatusController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
+@Import({ BookController.class, StatusController.class})
 public class SpringbootdeploymentsApplication {
+
+	@Bean
+	public HandlerMapping handlerMapping() {
+		return new RequestMappingHandlerMapping();
+	}
+
+	@Bean
+	public HandlerAdapter handlerAdapter() {
+		return new RequestMappingHandlerAdapter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdeploymentsApplication.class, args);
